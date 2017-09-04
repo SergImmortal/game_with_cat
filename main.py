08cd0d46c917448132c71f5cf2ee645a45d851be
Sergimmortal <s.maxymenko@gmail.com>
2017-09-04 21:36:31 +0300

    game engin v 0.1.1 add Camera modul

--- main.py
+++ main.py
@@ -3,21 +3,43 @@ from player import Player
 from levels import level
 from bloks import *
 
-
-WIN_WIDTH = 1920
-WIN_HEIGHT = 1040
-DISPLAY = (WIN_WIDTH, WIN_HEIGHT)
-BACKGROUND_COLOR = "#004400"
+# константы
+WIN_WIDTH = 640
+WIN_HEIGHT = 480
+DISPLAY = (WIN_WIDTH, WIN_HEIGHT) # Размер дисплея
+BACKGROUND_COLOR = "#004400" # Фон
+# Инициализация камеры
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+        
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+    
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+# настройки камеры        
+def camera_configuration(camera, terget_rect):
+    l, t, _, _ = terget_rect
+    _, _, w, h = camera
+    l, t = -l +WIN_WIDTH/2, -t + WIN_HEIGHT/2
+    
+    l = min(0, l)
+    l = max(-(camera.width-WIN_WIDTH), l)
+    t = max(-(camera.height-WIN_HEIGHT), t)
+    t = min (0, t)
+    return Rect(l, t, w, h)
 
 
 def main():
-    pg.init()
-    screen = pg.display.set_mode(DISPLAY)
-    pg.display.set_caption('My first game')
-    bg = pg.Surface(DISPLAY)
-    bg.fill(pg.Color(BACKGROUND_COLOR))
+    pg.init() # инициализируем пайгейм
+    screen = pg.display.set_mode(DISPLAY) # устанавливаем размер дисплея
+    pg.display.set_caption('My first game') #уснанавливаем название окна
+    bg = pg.Surface(DISPLAY) #создать дисплей
+    bg.fill(pg.Color(BACKGROUND_COLOR))#заливка дисплея фоном
     
-    timer = pg.time.Clock()
+    timer = pg.time.Clock() 
     
     hero = Player(55,55)
     left = right = False
@@ -39,7 +61,9 @@ def main():
         x = 0
     
         
-
+        total_level_width = len(level[0])*PLATFORM_WIDTH
+        total_level_height = len(level)*PLATFORM_HEIGHT
+        camera = Camera(camera_configuration, total_level_width, total_level_height)
     
     while True:
         timer.tick(35)
@@ -70,8 +94,11 @@ def main():
         
 
         hero.update(left, right, up, platforms_all)
-        entities.draw(screen)
+        camera.update(hero)
+        for e in entities:
+            screen.blit(e.image, camera.apply(e))
+            
         pg.display.update()
 
-if __name__ == "__main__":
+if __name__ == "__main__": #если окно открыто
     main()
